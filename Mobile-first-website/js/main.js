e7cea24d572d3fc19d089db343b19a18d612b28a
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-07-22 20:22:22 +0100

    Add files via upload

---
+++ Mobile-first-website/js/main.js
@@ -0,0 +1,16 @@
+// Author: Kyan Keise
+// Date:
+// Title: 
+
+// Animate smooth scroll
+
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
