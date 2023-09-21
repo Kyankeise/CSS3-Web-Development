b3703b8567fd4400f5a3042315ee72ff7bbfec68
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:20:00 +0100

    Add files via upload

--- Mobile-first-website/js/main.js
+++ Mobile-first-website/js/main.js
@@ -1,16 +1,16 @@
-// Author: Kyan Keise 
-// Date: 25/03/2022 
-// Title: Mobile First Website 
+// Author: Kyan Keise
+// Date:  29/04/2022
+// Title: Mobile First Website
 
 // Animate smooth scroll
 
-$('#view-work').on('click', function() {
-    const images = $('#images').position().top;
+$("#view-work").on("click", function () {
+  const images = $("#images").position().top;
 
-    $('html, body').animate(
-        {
-            scrollTop: images
-        },
-        900
-    );
+  $("html, body").animate(
+    {
+      scrollTop: images,
+    },
+    900
+  );
 });
