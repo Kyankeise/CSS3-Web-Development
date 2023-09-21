18ca05b7fba850cabb0a143bb56090e9679affc3
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:30:42 +0100

    Add files via upload

--- Sliding-Sign in form/HTML/js/main.js
+++ Sliding-Sign in form/HTML/js/main.js
@@ -1,21 +1,17 @@
 // Author: Kyan Keise
-// Date: 25/03/2022
+// Date: 27/04/2022
 // Title: Sliding Sign in & Sign up Form
 
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () =>
-container.classList.add("right-panel-active")
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
 );
 
-signInButton.addEventListener('click', () =>
-container.classList.add("right-panel-active")
+signInButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
 );
-
-
 
 
